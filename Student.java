8d0e191bc5c480dcbe612cda5375205fb1a74121
Albaraa Mashal <77306932+AlbaraaM99@users.noreply.github.com>
2025-03-26 19:54:59 +0300

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    private String name;
+    private ExamSchedule examSchedule;
+
+    public Student(String name) {
+        this.name = name;
+        this.examSchedule = new ExamSchedule();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ExamSchedule getExamSchedule() {
+        return examSchedule;
+    }
